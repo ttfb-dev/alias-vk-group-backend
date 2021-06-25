1855c4c735cff520b03dbc66bca44d000d3253d1
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-06-25 10:10:37 +0300

    confirmation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json())
 app.post('/callback', async (req, res) => {
   const body = req.body;
   logger.debug('got body', {body})
+  if (body.hasOwnProperty('type')) {
+    res.status(200).send('b68706e2')
+    return ;
+  }
   res.status(200).send();
 })
 
