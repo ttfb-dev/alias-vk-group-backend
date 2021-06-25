27327473e628c35c58a3a743ee9a384ed660cbff
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-06-25 10:31:09 +0300

    check creds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import logger from './logger.js'
+import vkGroupHandler from './vkGroupHandler.js'
 
 const app = express()
 const port = 80
@@ -9,9 +10,15 @@ app.use(bodyParser.json())
 app.post('/callback', async (req, res) => {
   const body = req.body;
   logger.debug('got body', {body})
-  if (body.hasOwnProperty('type')) {
-    res.status(200).send('b68706e2')
-    return ;
+  try {
+    if (vkGroupHandler.checkCred(body.group_id, body.secret)) {
+      const responseText = await vkGroupHandler.processRequest(body);
+      res.status(200).send(responseText ?? '');
+      return ;
+    }
+    throw new Error('Wrong credentials');
+  } catch ({ message }) {
+    logger.critical(message, {body});
   }
   res.status(200).send();
 })
---
+++ src/vkGroupHandler.js
@@ -0,0 +1,23 @@
+import logger from "./logger.js";
+
+const vkGroupHandler = {
+  checkCred: (group, secret) => {
+    return group === parseInt(process.env.GROUP_ID) && secret === process.env.GROUP_SECRET;
+  },
+
+  processRequest: async (body) => {
+    if (!body.hasOwnProperty(type)) {
+      logger.debug('got untyped request', {handler: 'vkGroupHandler.processRequest', body})
+      return ;
+    }
+    const {type} = body;
+    switch (type) {
+      case 'confirmation':
+        return 'b68706e2';
+      default:
+        await logger.debug('got new type', body);
+    }
+  }
+}
+
+export default vkGroupHandler;
