e84eb65fce86aee1313466ff305591665e99fd54
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-21 18:11:25 +0300

    upd return conde

--- src/vkGroupHandler.js
+++ src/vkGroupHandler.js
@@ -1,53 +1,64 @@
 import logger from "./logger.js";
 import prs from "./prs.js";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 const vkGroupHandler = {
   checkCred: (group, secret) => {
-    return group === parseInt(process.env.GROUP_ID) && secret === process.env.GROUP_SECRET;
+    return (
+      group === parseInt(process.env.GROUP_ID) &&
+      secret === process.env.GROUP_SECRET
+    );
   },
 
   processRequest: async (body) => {
-    if (!body.hasOwnProperty('type')) {
-      logger.critical('got untyped request', {handler: 'vkGroupHandler.processRequest', body})
-      return 'ok';
+    if (!body.hasOwnProperty("type")) {
+      logger.critical("got untyped request", {
+        handler: "vkGroupHandler.processRequest",
+        body,
+      });
+      return "ok";
     }
     const { type, object } = body;
     let needRefreshDatasets = false;
     switch (type) {
-      case 'confirmation':
-        return 'b68706e2';
-      case 'group_leave':
-        await prs.setUserParam(object.user_id, 'is_group_member', false);
+      case "confirmation":
+        return "688064d5";
+      case "group_leave":
+        await prs.setUserParam(object.user_id, "is_group_member", false);
         needRefreshDatasets = true;
         break;
-      case 'group_join':
-        await prs.setUserParam(object.user_id, 'is_group_member', true);
+      case "group_join":
+        await prs.setUserParam(object.user_id, "is_group_member", true);
         needRefreshDatasets = true;
         break;
-      case 'donut_subscription_create':
-        await prs.setUserParam(object.user_id, 'is_donut', true);
-        await prs.setUserParam(object.user_id, 'donut_value', object.amount);
+      case "donut_subscription_create":
+        await prs.setUserParam(object.user_id, "is_donut", true);
+        await prs.setUserParam(object.user_id, "donut_value", object.amount);
         needRefreshDatasets = true;
         break;
-      case 'donut_subscription_expired':
-        await prs.setUserParam(object.user_id, 'is_donut', false);
-        await prs.delUserParam(object.user_id, 'donut_value');
+      case "donut_subscription_expired":
+        await prs.setUserParam(object.user_id, "is_donut", false);
+        await prs.delUserParam(object.user_id, "donut_value");
         needRefreshDatasets = true;
         break;
       default:
-        await logger.debug('got unused type', body);
+        await logger.debug("got unused type", body);
         break;
     }
     if (needRefreshDatasets) {
       try {
-        await fetch(`http://logux-proxy:9000/user/${object.user_id}/refresh-datasets`);
+        await fetch(
+          `http://logux-proxy:9000/user/${object.user_id}/refresh-datasets`
+        );
       } catch ({ message }) {
-        logger.critical(message, { method: 'vkGroupHandler.processRequest', body})
-      } 
+        logger.critical(message, {
+          method: "vkGroupHandler.processRequest",
+          body,
+        });
+      }
     }
-    return 'ok';
-  }
-}
+    return "ok";
+  },
+};
 
 export default vkGroupHandler;
