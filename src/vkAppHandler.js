0ee9972d10bc9b3811abaf3c1d695b354ab2d5c3
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-23 09:46:32 +0300

    upd

--- src/vkAppHandler.js
+++ src/vkAppHandler.js
@@ -7,7 +7,7 @@ const subscriptions = {
     title: "Подписка на все наборы слов",
     price: 10,
     period: 30,
-    trial_duration: 0,
+    trial_duration: 3,
     photo_url:
       "https://st2.depositphotos.com/1064024/10769/i/600/depositphotos_107694484-stock-photo-little-boy.jpg",
   },
